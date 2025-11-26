79b13e59a0e3b37dc9cb417ec8ba4f86025b27b9
ejchiluiza <ejchiluiza@gmail.com>
2025-11-25 20:03:04 -0500

    Agregando script.js faltante

---
+++ script.js
@@ -0,0 +1,9 @@
+//Enlaces a los componentes
+
+import './src/components/dashboard.js';
+import './src/components/FormularioLibro.js';
+import './src/components/ListaLibros.js';
+import './src/components/ItemLibro.js';
+import './src/components/TarjetaEstadisticas.js';
+import './src/components/VentanaConfirmacion.js';
+import './src/components/PrestamoLibro.js';
